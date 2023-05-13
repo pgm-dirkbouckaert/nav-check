9edb0c530c677a5a0ce4ea9e7672c6a8d98ce04b
pgm-dirkbouckaert <dirk.bouckaert@student.arteveldehs.be>
2023-05-13 16:39:49 +0200

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,117 @@
+(() => {
+  const app = {
+    init() {
+      this.language = null;
+      this.cacheElements();
+      this.getLanguage();
+      this.buildUI();
+      this.listenForChangeLanguage();
+      this.listenForSubmitForm();
+      this.listenForResetForm();
+    },
+    cacheElements() {
+      this.$chooseLanguage = document.getElementById('choose-language');
+      this.$form = document.getElementById('form');
+      this.$checklist = document.getElementById('checklist');
+      this.$btnReset = document.getElementById('btn-reset');
+      this.$result = document.getElementById('result');
+    },
+    /**
+     * GET LANGUAGE
+     */
+    getLanguage() {
+      this.language = localStorage.getItem('language');
+      if (this.language) this.$chooseLanguage.value = this.language;
+      else {
+        this.language = this.$chooseLanguage.value;
+        localStorage.setItem('language', this.language);
+      }
+    },
+    listenForChangeLanguage() {
+      this.$chooseLanguage.addEventListener('change', (e) => {
+        e.preventDefault();
+        const newLang = e.currentTarget.value;
+        localStorage.setItem('language', newLang);
+        this.language = newLang;
+        this.buildUI();
+      });
+    },
+    /**
+     * BUILD UI
+     */
+    buildUI() {
+      const categories = [
+        ...new Set(data.map((item) => item.category[this.language])),
+      ];
+      this.$checklist.innerHTML = this.renderChecklist(categories);
+    },
+    renderChecklist(categories) {
+      return categories
+        .map((category) => {
+          const filteredData = data.filter(
+            (item) => item.category[this.language] === category
+          );
+          return `
+            <h3 class="border rounded bg-light shadow-sm p-2 mt-3">${category}</h3>
+            ${filteredData
+              .map((item, index) => {
+                return `
+                <div class="form-check fs-5">
+                  <input  type="checkbox"
+                      class="form-check-input"
+                      name="${this.getSlug(item.text[this.language])}" 
+                      id="check-${index}"
+                      value="${item.score}" />
+                  <label for="check-${index}" class="form-check-label">
+                    ${item.text[this.language]}
+                  </label>
+              </div>`;
+              })
+              .join('')}
+
+          `;
+        })
+        .join('');
+    },
+    listenForSubmitForm() {
+      this.$form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let totalScore = 0;
+        const checkboxes = document.querySelectorAll('input[type=checkbox]');
+        for (const box of checkboxes) {
+          if (box.checked) totalScore += parseInt(box.value);
+        }
+        const percent = totalScore * 4;
+        this.$result.innerHTML = `
+        <span class="text-${this.getResultColor(percent)}">
+          Score: ${percent}%</span>`;
+        this.$result.classList.remove('d-none');
+      });
+    },
+    listenForResetForm() {
+      this.$btnReset.addEventListener('click', (e) => {
+        e.preventDefault();
+        const checkboxes = document.querySelectorAll('input[type=checkbox]');
+        for (const box of checkboxes) {
+          box.checked = false;
+        }
+        this.$result.innerHTML = '';
+        this.$result.classList.toggle('d-none');
+      });
+    },
+    getSlug(str) {
+      // Source: https://www.linkedin.com/learning/javascript-code-challenges/urlify
+      return str
+        .trim()
+        .toLowerCase()
+        .replace(/[?,.;:!/\\+=´`'"]/g, '')
+        .replace(/\s/g, '-');
+    },
+    getResultColor(percent) {
+      if (percent >= 75) return 'success';
+      if (percent >= 60) return 'warning';
+      if (percent < 60) return 'danger';
+    },
+  };
+  app.init();
+})();
---
+++ app/data.js
@@ -0,0 +1,139 @@
+const data = [
+  {
+    category: { en: 'Navigation menu', nl: 'Navigatiemenu' },
+    text: {
+      en: 'Visible and easy to find.',
+      nl: 'Zichtbaar en gemakkelijk vindbaar.',
+    },
+    score: 2,
+  },
+  {
+    category: { en: 'Navigation menu', nl: 'Navigatiemenu' },
+    text: { en: 'Mobile friendly.', nl: 'Mobielvriendelijk.' },
+    score: 2,
+  },
+  {
+    category: { en: 'Navigation menu', nl: 'Navigatiemenu' },
+    text: {
+      en: 'Limited number of menu items and submenus.',
+      nl: "Beperkt aantal menu-items en submenu's.",
+    },
+    score: 2,
+  },
+  {
+    category: { en: 'Navigation menu', nl: 'Navigatiemenu' },
+    text: {
+      en: 'Concise and descriptive labels.',
+      nl: 'Beknopte en beschrijvende labels.',
+    },
+    score: 2,
+  },
+  {
+    category: { en: 'Navigation menu', nl: 'Navigatiemenu' },
+    text: {
+      en: 'Labels without jargon or technical terms.',
+      nl: 'Labels zonder jargon of technische termen.',
+    },
+    score: 2,
+  },
+  {
+    category: { en: 'Navigation menu', nl: 'Navigatiemenu' },
+    text: {
+      en: 'Uses icons and/or colors for categories or levels of hierarchy.',
+      nl: 'Gebruikt pictogrammen en/of kleuren voor categorieën of niveaus van hiërarchie.',
+    },
+    score: 1,
+  },
+  {
+    category: { en: 'Search functionality', nl: 'Zoekfunctionaliteit' },
+    text: {
+      en: 'Clearly present.',
+      nl: 'Duidelijk aanwezig.',
+    },
+    score: 2,
+  },
+  {
+    category: { en: 'Search functionality', nl: 'Zoekfunctionaliteit' },
+    text: {
+      en: 'Present on every page.',
+      nl: 'Aanwezig op elke pagina.',
+    },
+    score: 1,
+  },
+  {
+    category: { en: 'Search functionality', nl: 'Zoekfunctionaliteit' },
+    text: {
+      en: 'Contains autocomplete.',
+      nl: 'Bevat automatisch aanvullen.',
+    },
+    score: 1,
+  },
+  {
+    category: { en: 'Search functionality', nl: 'Zoekfunctionaliteit' },
+    text: {
+      en: 'Contains filters.',
+      nl: 'Bevat filters.',
+    },
+    score: 1,
+  },
+  {
+    category: { en: 'Search functionality', nl: 'Zoekfunctionaliteit' },
+    text: {
+      en: 'You can search in your own words.',
+      nl: 'Je kan zoeken met eigen woorden.',
+    },
+    score: 1,
+  },
+  {
+    category: { en: 'Search functionality', nl: 'Zoekfunctionaliteit' },
+    text: {
+      en: 'The search results are relevant.',
+      nl: 'De zoekresultaten zijn relevant.',
+    },
+    score: 2,
+  },
+  {
+    category: {
+      en: 'Other navigation options',
+      nl: 'Andere navigatiemogelijkheden',
+    },
+    text: {
+      en: 'Throughout the website, pages link to related content.',
+      nl: 'Doorheen de website wordt er op pagina’s gelinkt naar gerelateerde inhoud.',
+    },
+    score: 2,
+  },
+  {
+    category: {
+      en: 'Other navigation options',
+      nl: 'Andere navigatiemogelijkheden',
+    },
+    text: {
+      en: "The website offers prominent calls to action in relevant places (e.g. 'Read more' or 'More info').",
+      nl: 'De website biedt op relevante plaatsen prominente call-to-actions aan (bv ‘Lees meer’ of ‘Meer info’).',
+    },
+    score: 2,
+  },
+  {
+    category: {
+      en: 'Other navigation options',
+      nl: 'Andere navigatiemogelijkheden',
+    },
+    text: {
+      en: 'The website offers breadcrumbs.',
+      nl: 'De website biedt een kruimelpad (breadcrumbs) aan.',
+    },
+    score: 1,
+  },
+  {
+    category: {
+      en: 'Other navigation options',
+      nl: 'Andere navigatiemogelijkheden',
+    },
+    text: {
+      en: 'The website offers a sitemap or category page.',
+      nl: 'De website biedt een sitemap of categoriepagina aan.',
+    },
+    score: 1,
+  },
+];
